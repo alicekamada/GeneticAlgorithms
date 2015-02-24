7cea20956bae0520e78bbf6c228c451b209372b6
Cassandra Cruz <alicekamada@users.noreply.github.com>
2015-02-24 11:26:18 -0800

    Update geneticSalesman.js
    Implemented geneticSalesman() and alterRoute()

--- src/geneticSalesman.js
+++ src/geneticSalesman.js
@@ -4,10 +4,33 @@ var geneticSalesman = function(genes, assessFitness, initiateBloodline, mutate, 
     offspringPerSurvivor: 50,
   };
 
+  var optimalRoute;
+
   /* -------------------- Complete me! -------------------- */
+  for (var i = 0; i <  options.numberOfBloodlines; i++) {
+    var bloodline = initiateBloodline(genes);
+    var survivor;
+
+    for (var j = 0; j < availableResources; j++) {
+      for (var k = 0; k < options.offspringPerSurvivor; k++) {
+        var child = mutate(bloodline);
+        if (survivor === undefined || (assessFitness(child) < assessFitness(survivor))) {
+          survivor = child;
+        }
+      }
+      bloodline = survivor;
+    }
+
+
+    if (optimalRoute === undefined || (assessFitness(bloodline) < assessFitness(optimalRoute))) {
+      optimalRoute = bloodline;
+    }
+
+  }
+
 
   return optimalRoute;
-}
+};
 
 var createRoute = function(cities){
   var route = cities.slice();
@@ -17,13 +40,23 @@ var createRoute = function(cities){
     route[randomIndex] = cities[i];
   }
   return route;
-}
+};
 
 var alterRoute = function(route){
 
   /* -------------------- Complete me! -------------------- */
+  var newRoute = route.slice();
 
-}
+  for (var i = 0; i < 2; i++) {
+    var index1 = Math.floor(Math.random() * newRoute.length);
+    var index2 = Math.floor(Math.random() * newRoute.length);
+    var temp = newRoute[index1];
+    newRoute[index1] = newRoute[index2];
+    newRoute[index2] = temp;
+  }
+
+  return newRoute;
+};
 
 var calculateDistance = function(route){
   var distances = route.map(function(city, index, route){
@@ -35,5 +68,4 @@ var calculateDistance = function(route){
   return distances.reduce(function(distance1, distance2){
     return distance1 + distance2;
   });
-}
-
+};

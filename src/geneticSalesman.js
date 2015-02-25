96d2d39f239c4461255771c28c876a27401dad21
Cassandra Cruz <celkamada@gmail.com>
2015-02-24 17:47:13 -0800

    Web workers are working as intended, have not figured out how to get function to wait for them.

--- src/geneticSalesman.js
+++ src/geneticSalesman.js
@@ -4,10 +4,41 @@ var geneticSalesman = function(genes, assessFitness, initiateBloodline, mutate, 
     offspringPerSurvivor: 50,
   };
 
-  /* -------------------- Complete me! -------------------- */
+  var optimalRoute;
+  var bloodlinesDone = 0;
 
-  return optimalRoute;
-}
+  /* -------------------- Complete me! -------------------- */
+  for (var i = 0; i <  options.numberOfBloodlines; i++) {
+    var bloodline = initiateBloodline(genes);
+    var survivor;
+
+    var worker = new Worker('./src/worker.js');
+
+    var receiveMessage = function (e) {
+      console.log(e, bloodlinesDone);
+
+
+      if (optimalRoute === undefined || (assessFitness(e.data.survivor) < assessFitness(optimalRoute))) {
+        optimalRoute = e.data.survivor.slice();
+      }
+
+      bloodlinesDone++;
+
+      if (bloodlinesDone === options.numberOfBloodlines) {
+        console.log(optimalRoute);
+        return optimalRoute;
+      }
+    };
+
+    worker.addEventListener('message', receiveMessage);
+
+    worker.postMessage({
+      availableResources: availableResources,
+      offspringPerSurvivor: options.offspringPerSurvivor,
+      bloodline: bloodline
+    });
+  }
+};
 
 var createRoute = function(cities){
   var route = cities.slice();
@@ -17,13 +48,23 @@ var createRoute = function(cities){
     route[randomIndex] = cities[i];
   }
   return route;
-}
+};
 
 var alterRoute = function(route){
 
   /* -------------------- Complete me! -------------------- */
+  var newRoute = route.slice();
 
-}
+  for (var i = 0; i < 2; i++) {
+    var index1 = Math.floor(Math.random() * newRoute.length);
+    var index2 = Math.floor(Math.random() * newRoute.length);
+    var temp = newRoute[index1];
+    newRoute[index1] = newRoute[index2];
+    newRoute[index2] = temp;
+  }
+
+  return newRoute;
+};
 
 var calculateDistance = function(route){
   var distances = route.map(function(city, index, route){
@@ -35,5 +76,4 @@ var calculateDistance = function(route){
   return distances.reduce(function(distance1, distance2){
     return distance1 + distance2;
   });
-}
-
+};
